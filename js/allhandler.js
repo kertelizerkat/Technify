43aea5f139cf7e723b71e65aac75c44c8999e69e
Kertelizerkat <114565637+kertelizerkat@users.noreply.github.com>
2025-03-22 17:45:54 +0200

    Add files via upload
    Signed-off-by: Kertelizerkat <114565637+kertelizerkat@users.noreply.github.com>

--- js/allhandler.js
+++ js/allhandler.js
@@ -1,6 +1,6 @@
 
 let line="https://sculpin-charming-directly.ngrok-free.app"
 // line="http://localhost:5700"
-let theme_m='Dark'
-localStorage.setItem('theme_+-----', theme_m)
+// let theme_m='Dark'
+// localStorage.setItem('theme_+-----', theme_m)
 let access_="domContent"
