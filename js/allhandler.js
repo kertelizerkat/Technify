98087f12d4bee0bc079d2381d5f9837c4004626a
Kertelizerkat <114565637+kertelizerkat@users.noreply.github.com>
2025-03-29 20:43:23 +0200

    Update allhandler.js
    Signed-off-by: Kertelizerkat <114565637+kertelizerkat@users.noreply.github.com>

--- js/allhandler.js
+++ js/allhandler.js
@@ -1,7 +1,7 @@
 
 let line="https://sculpin-charming-directly.ngrok-free.app"
-line="http://localhost:5700"
-let theme_m='Light'
+// line="http://localhost:5700"
+let theme_m='Dark'
 localStorage.setItem('theme_+-----', theme_m)
 let access_="domContent"
 let online=false
