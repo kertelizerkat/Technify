8e3850be0dd164247199fa5079c6aebadec098c2
Kertelizerkat <114565637+kertelizerkat@users.noreply.github.com>
2025-04-05 10:50:18 +0200

    Add files via upload
    Signed-off-by: Kertelizerkat <114565637+kertelizerkat@users.noreply.github.com>

--- Logged/js/verify.js
+++ Logged/js/verify.js
@@ -1,5 +1,5 @@
 
-window.addEventListener('load',()=>{
+window.addEventListener('DOMContentLoaded',()=>{
     let dmcon = accessInfo("domContent2")
     let givendt = accessInfo('domContentdate2')
     let ons= accessInfo('online')
@@ -117,6 +117,41 @@ window.addEventListener('load',()=>{
     waitForSixtySeconds();
     location.reload(false)
    }}
+
+   // Side Menu Toggle Script
+let menu_img = document.querySelector(".menu");
+let menu = document.querySelector(".closed");
+
+menu_img.addEventListener("click", () => {
+    if (menu_img.textContent == "Menu") {
+        menu_img.textContent = "Close";
+    } else {
+        menu_img.textContent = "Menu";
+    }
+    menu_img.classList.toggle("pink");
+
+    menu.classList.toggle("open");
+
+
+})
+ 
+
+const header = document.querySelector("header");
+let val=0;
+let w= window.scrollY
+window.addEventListener("scroll", () => {
+  if (window.scrollY > w) {
+    val=val+2;
+    header.style.backgroundColor = "rgba(0,0,0,0."+ String(val)+ ")";
+    w= window.scrollY
+  } else {
+    val=val-2;
+    header.style.backgroundColor = "rgba(0,0,0,0."+ String(val)+ ")";
+    w= window.scrollY
+  }
+});
+
+
   
 })
 
